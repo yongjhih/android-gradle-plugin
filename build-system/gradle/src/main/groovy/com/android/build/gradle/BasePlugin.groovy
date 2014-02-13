c3a3a51122882b471414bc5a2303d4dec2ed867c
Xavier Ducrohet <xav@android.com>
2014-02-13 05:39:23 +0000

    Revert "Tweak the exploded bundle folder."
    This reverts commit f3e7d672859ce2e93fe5618ec98081fd5771d476.
    
    Change-Id: Ic485fac8083eb3142d9b04c4e64a450500a20b26

--- build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -2055,8 +2055,9 @@ public abstract class BasePlugin {
             def moduleArtifacts = artifacts[id]
             moduleArtifacts?.each { artifact ->
                 if (artifact.type == EXT_LIB_ARCHIVE) {
+                    String bundleName = GUtil.toCamelCase(id.group + " " + id.name + " " + id.version)
                     def explodedDir = project.file(
-                            "$project.buildDir/exploded-aar/$id.group/$id.name/$id.version")
+                            "$project.buildDir/exploded-bundles/${bundleName}.aar")
                     LibraryDependencyImpl adep = new LibraryDependencyImpl(
                             artifact.file, explodedDir, nestedBundles,
                             id.group + ":" + id.name + ":" + id.version)
